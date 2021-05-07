9d79cab4192fcbbc019deadc881304d780a3754d
yildirimGulay <gulaycontact@gmail.com>
2021-05-07 13:41:04 +0100

    axios, fontawesome aded

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 `yarn add -D gh-pages`
 
+`yarn add axios`
+
+
+`yarn add font-awesome`
+<!--fontawesom-->
+    <script src="https://kit.fontawesome.com/c4c0c2a8bd.js" crossorigin="anonymous"></script>
--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import axios from 'axios';
+/** @format */
+
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
   // use the api
   const [weather, setWeather] = useState([]);
 
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState("");
 
-  const [query, setQuery] = useState('London');
+  const [query, setQuery] = useState("London");
 
   //Weather Show
-  const [icon, setIcon] = useState('');
-  const [country, setCountry] = useState('');
-  const [temp, setTemp] = useState('');
-  const [main, setMain] = useState('');
+  const [icon, setIcon] = useState("");
+  const [country, setCountry] = useState("");
+  const [temp, setTemp] = useState("");
+  const [main, setMain] = useState("");
 
   //form
   const handleClick = (e) => {
     e.preventDefault();
 
-    console.log('clicklendi');
+    console.log("clicklendi");
 
     setQuery(term);
   };
@@ -40,21 +42,20 @@ function App() {
         setTemp(response.data.main.temp);
         setMain(response.data.weather[0].main);
 
-        console.log('hooksearchterm', query);
+        console.log("hooksearchterm", query);
       })();
     } catch (err) {
       console.log(err);
     }
   }, [query]);
 
-  console.log('app weather', weather);
+  console.log("app weather", weather);
 
   return (
-    <>
+    
       <div className="App">
         <div className="card">
           <h2 className="title">
-            {' '}
             <i className="fa fa-cloud"></i>Weather App
           </h2>
 
@@ -66,24 +67,24 @@ function App() {
               placeholder="Enter your cityname"
             />
             <button type="button" onClick={handleClick}>
-              {' '}
-              Search{' '}
+              {" "}
+              Search{" "}
             </button>
           </form>
           <h2>{main}</h2>
           <img
             style={{ width: 150, height: 150 }}
-            src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+            src={`http://openweathermap.org/img/w/${icon}.png`}
             alt="icon"
           />
           <h2>{Math.floor(temp)}&deg;</h2>
           <h2>
-            {' '}
+            {" "}
             {weather.name}, {country}
           </h2>
         </div>
       </div>
-    </>
+  
   );
 }
 
